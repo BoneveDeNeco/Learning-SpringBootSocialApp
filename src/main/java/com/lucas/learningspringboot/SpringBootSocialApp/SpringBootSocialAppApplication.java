2288b693db5dd29e1d35b61380476f028137ef44
Lucas Branisso <LUCASB@amdocs.com>
2018-09-13 18:43:28 -0300

    Added index html markup and css

--- src/main/java/com/lucas/learningspringboot/SpringBootSocialApp/SpringBootSocialAppApplication.java
+++ src/main/java/com/lucas/learningspringboot/SpringBootSocialApp/SpringBootSocialAppApplication.java
@@ -2,11 +2,19 @@ package com.lucas.learningspringboot.SpringBootSocialApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringBootSocialAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSocialAppApplication.class, args);
+	}
+	
+	//Delete is not a valid action in HTML5 form, but this makes thymeleaf do a small workaround to support it
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 }
